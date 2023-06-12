faaee1070ddef2cd6cc03ea436162c624c73fe24
fmonpelat <facundo.monpelat@gmail.com>
2023-06-12 20:11:07 -0300

    added public functions to interface of IGameMode

--- core/src/main/java/com/core/g3/Match/GameMode/IGameMode.java
+++ core/src/main/java/com/core/g3/Match/GameMode/IGameMode.java
@@ -1,9 +1,23 @@
 package com.core.g3.Match.GameMode;
 
+import java.util.Optional;
+
+import com.core.g3.Deck.IDeck;
+import com.core.g3.Match.Player.Player;
+import com.core.g3.User.User;
+
 public interface IGameMode {
     public int CombatZoneLimit();
 
     public int ArtifactZoneLimit();
 
     public int ReserveZoneLimit();
+
+    public Optional<Player> getWinner(Player player1, Player player2);
+
+    public void drawInitialCards(Player player);
+
+    public Player addPlayer(User user, IDeck deck);
+
+    public void initialStage(Player player);
 }
