f41aa55cd8d9a854e86fcac55878775184ba6e42
fmonpelat <facundo.monpelat@gmail.com>
2023-06-14 22:35:09 -0300

    added alquimist card

--- core/src/main/java/com/core/g3/Card/Cards/Alchemist.java
+++ core/src/main/java/com/core/g3/Card/Cards/Alchemist.java
@@ -2,16 +2,15 @@ package com.core.g3.Card.Cards;
 
 import java.util.ArrayList;
 import java.util.Optional;
-import java.util.concurrent.ArrayBlockingQueue;
 
 import com.core.g3.Card.Card;
 import com.core.g3.Card.CardBuilder;
 import com.core.g3.Card.CardName;
-import com.core.g3.Card.Artefact.AddEnergyArtefact;
 import com.core.g3.Card.Artefact.DrawCardArtefact;
 import com.core.g3.Card.Attack.AddEnergyAttack;
 import com.core.g3.Card.Attack.DrawCardAttack;
 import com.core.g3.Card.Attack.IAttack;
+import com.core.g3.Card.Attack.SimpleAttack;
 import com.core.g3.Card.Type.Creature.Attribute;
 import com.core.g3.Commons.Amount;
 import com.core.g3.Match.Player.Resources.EnergyType;
@@ -27,8 +26,8 @@ public class Alchemist {
         creatureAttributes.add(Attribute.Science);
 
         ArrayList<IAttack> creatureAttacks = new ArrayList<>();
-        creatureAttacks.add(new DrawCardAttack()); // TODO: ver como pasarle el amount de daño que causa y cantidad de cartas
-        creatureAttacks.add(new AddEnergyAttack(EnergyType.Fire, new Amount(1))); // TODO: agregar cuanto daño hace
+        creatureAttacks.add(new DrawCardAttack(new SimpleAttack(new Amount(1))));
+        creatureAttacks.add(new AddEnergyAttack(EnergyType.Fire, new Amount(1), new DrawCardAttack()));
 
         builder.cardTypeBuilder.setTypeCreature(new Amount(3), creatureAttributes, creatureAttacks);
 
