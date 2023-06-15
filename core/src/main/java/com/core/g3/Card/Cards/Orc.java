951df04b6ba3a91227b016ab4814508aaabe2a30
fmonpelat <facundo.monpelat@gmail.com>
2023-06-14 23:09:27 -0300

    removed unused imports

--- core/src/main/java/com/core/g3/Card/Cards/Orc.java
+++ core/src/main/java/com/core/g3/Card/Cards/Orc.java
@@ -1,17 +1,12 @@
 package com.core.g3.Card.Cards;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 import com.core.g3.Card.Card;
 import com.core.g3.Card.CardBuilder;
 import com.core.g3.Card.CardName;
-import com.core.g3.Card.Action.DamageAction;
-import com.core.g3.Card.Action.IAction;
 import com.core.g3.Card.Attack.IAttack;
 import com.core.g3.Card.Attack.SimpleAttack;
-import com.core.g3.Card.Cost.CostEnergy;
-import com.core.g3.Card.Cost.ICost;
 import com.core.g3.Card.Type.Creature.Attribute;
 import com.core.g3.Commons.Amount;
 import com.core.g3.Match.Player.Resources.EnergyType;
